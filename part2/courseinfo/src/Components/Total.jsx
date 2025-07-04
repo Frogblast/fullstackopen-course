8fa2a972a7fcd18c790209c94e483c008538fd4b
Petter Reingardt <petter.reingardt@gmail.com>
2025-07-04 15:59:10 +0200

    2.5 done

--- part2/courseinfo/src/Components/Total.jsx
+++ part2/courseinfo/src/Components/Total.jsx
@@ -1,6 +1,7 @@
 export const Total = ({ parts }) => {
     const exercises = parts.map(part => part.exercises)
     const sum = exercises.reduce((accumulator, currentValue) => accumulator + currentValue)
+    
     return (
         <div>
             <h2>Total</h2>
