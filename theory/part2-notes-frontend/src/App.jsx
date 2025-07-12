f14b29b8c0e5862cdc630313b25163d9d57e1834
Petter Reingardt <petter.reingardt@gmail.com>
2025-07-12 12:19:08 +0200

    2.11 done

--- theory/part2-notes-frontend/src/App.jsx
+++ theory/part2-notes-frontend/src/App.jsx
@@ -1,17 +1,29 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
-import { useState } from 'react'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState(
-    "A new note..."
-  )
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }
+  useEffect(hook, [])
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
-    
+
     const noteObject = {
       id: String(notes.length + 1),
       content: newNote,
